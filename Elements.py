4a85216430bbd2ea7852096ba114a4a28b1486bd
tekq <81379378+tekq@users.noreply.github.com>
2022-01-08 20:11:11 +0200

    Elements One Alpha 3
    Welcome to Elements One Alpha 3, where install now works, kinda, search is broken as always, i might as well just not use it :/, and most functions are not even close to done, but at least install works, also search has been broken in EVERY VERSION THAT HAD IT, THAT'S IMPRESSIVE

--- Elements.py
+++ Elements.py
@@ -1,5 +1,4 @@
-import os
-import sys
+import os, sys
 from colorama import Fore
 
 ver = "one α"
@@ -7,17 +6,27 @@ ver = "one α"
 branch = "next"
 
 def search_repository():
-    local_repo_contains = os.system("/etc/elements/search-repo " + sys.argv[2])
+    global local_repo_contains
+    global pacman
+    pacman = False
+    local_repo_contains = os.system("/etc/elements/search-repo " + sys.argv[2] + " >> /dev/null")
     if local_repo_contains != 0:
-        if os.system("pacman -S " + " ".join(sys.argv[2:])) != 0:
-            print("BRUH")
+        if os.system("pacman -Ss " + " ".join(sys.argv[2:]) + " >> /dev/null") != 0:
+            sys.exit()
+        else:
+            pacman = True
+    else:
+        local_repo_contains = os.popen("/etc/elements/search-repo " + sys.argv[2]).read()
+        local_repo_contains = local_repo_contains.replace('\n', ' ')
+        local_repo_contains = local_repo_contains.split(' ', 1)
+        local_repo_contains = local_repo_contains[0]
+        print(local_repo_contains)
 
 
 def chk_root():
     if os.geteuid() != 0:
         print("Root is required to run " + sys.argv[1])
         sys.exit()
-
 
 if not sys.argv[1:]:
     print("Elements " + ver)
@@ -34,14 +43,20 @@ else:
         if not sys.argv[2:]:
             print("Must Specify what package to " + sys.argv[1] + ".")
             sys.exit()
+
 if sys.argv[1] in "install":
     chk_root()
+    search_repository()
     print("The following packages will be installed:")
     print(" " + ", ".join(sys.argv[2:]))
     prompt = input(
         "Do you wish to continue? " + "[" + Fore.GREEN + "Y" + Fore.RESET + "/" + Fore.RED + "n" + Fore.RESET + "] ")
-    if prompt in ["y", "yes"]:
-        print()
+    if prompt in ["y", "yes", ""]:
+        if pacman is True:
+            os.system("pacman -S --noconfirm " + sys.argv[2])
+            sys.exit()
+        os.system("/etc/elements/repos/" + local_repo_contains + "/" + sys.argv[2] + "/build")
+
         ## TODO: Add install scripts
     elif prompt in ["n", "no"]:
         print("Exit.")
@@ -53,19 +68,28 @@ elif sys.argv[1] in "remove":
     print(" " + ", ".join(sys.argv[2:]))
     prompt = input(
         "Do you wish to continue? " + "[" + Fore.GREEN + "Y" + Fore.RESET + "/" + Fore.RED + "n" + Fore.RESET + "] ")
-    if prompt in ["y", "yes"]:
+    if prompt in ["y", "yes", ""]:
         print()
         ## TODO: Add removal scripts
     elif prompt in ["n", "no"]:
         sys.exit()
 
 elif sys.argv[1] in "search":
-    os.system("/etc/elements/search " + sys.argv[2])
-    search_repository()
+    if os.system("/etc/elements/search-repo " + sys.argv[2] + " >> /dev/null") == 0:
+        print(sys.argv[2] + " found.")
+    else:
+        print("Could not find " + sys.argv[2])
+
 elif sys.argv[1] in "update":
     os.system("wget https://raw.githubusercontent.com/NitrogenLinux/elements/" + branch + "/Elements.py")
+## TODO: Complete update script
+## TODO: Make update script use the binary file instead of the source code
+
 elif sys.argv[1] in "show":
-    print()
-    ## TODO: Add Show Package Mechanism
+    if os.system("./search " + sys.argv[2] + " >> /dev/null") != 0:
+        print(sys.argv[2] + " not found.")
+    else:
+        print("Package: " + sys.argv[2])
+        print("Repository: " + os.popen("./search-repo " + sys.argv[2]).read())
 else:
     print(sys.argv[1] + ": Command Not found.")
