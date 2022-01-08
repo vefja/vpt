d3a0f53a840e8b82c5b10104197f15814e8ff649
tekq <81379378+tekq@users.noreply.github.com>
2022-01-08 21:59:01 +0200

    Elements One β1
    New and Improved Elements is now in Beta(β)!
    
    Now with: 
    finished install functions with multiple package support
    New Remove function which removes any package
    WIP Updater(not finished)
    Updated Show
    Search repository now searches for a specific package(instead of the 1st package that is listed)

--- Elements.py
+++ Elements.py
@@ -1,25 +1,27 @@
 import os, sys
 from colorama import Fore
 
-ver = "one α"
+ver = "one β"
 ## TODO: change next to stable when Elements One gets released
 branch = "next"
 
+package_install = 2
 
 def search_repository():
     global local_repo_contains
     global pacman
     global success
+    global packages_to_install
+    global package_install
     pacman = False
-    local_repo_contains = os.system("/etc/elements/search-repo " + sys.argv[2] + " >> /dev/null")
+    local_repo_contains = os.system("/etc/elements/search-repo " + sys.argv[package_install] + " >> /dev/null")
     success = local_repo_contains
     if local_repo_contains != 0:
-        if os.system("pacman -Ss " + " ".join(sys.argv[2:]) + " >> /dev/null") != 0:
-            sys.exit()
-        else:
-            pacman = True
+        ##if os.system("pacman -Ss " + " ".join(sys.argv[package_install]) + " >> /dev/null") != 0:
+         ##   sys.exit()
+        pacman = True
     else:
-        local_repo_contains = os.popen("/etc/elements/search-repo " + sys.argv[2]).read()
+        local_repo_contains = os.popen("/etc/elements/search-repo " + sys.argv[package_install]).read()
         local_repo_contains = local_repo_contains.replace('\n', ' ')
         local_repo_contains = local_repo_contains.split(' ', 1)
         local_repo_contains = local_repo_contains[0]
@@ -52,15 +54,24 @@ else:
 if sys.argv[1] in "install":
     chk_root()
     search_repository()
+    if len(sys.argv[2:]) != len(set(sys.argv[2:])):
+        sys.argv[2:] = list(dict.fromkeys(sys.argv[2:]))
     print("The following packages will be installed:")
     print(" " + ", ".join(sys.argv[2:]))
     prompt: str = input(
         "Do you wish to continue? " + "[" + Fore.GREEN + "Y" + Fore.RESET + "/" + Fore.RED + "n" + Fore.RESET + "] ")
     if prompt in ["y", "yes", ""]:
-        if pacman is True:
-            os.system("pacman -S --noconfirm " + sys.argv[2])
-            sys.exit()
-        os.system("/etc/elements/repos/" + local_repo_contains + "/" + sys.argv[2] + "/build")
+        packages_to_install = len(sys.argv[2:])
+        while packages_to_install > 0:
+            search_repository()
+            if pacman is True:
+                os.system("pacman -S --noconfirm " + sys.argv[package_install])
+                packages_to_install = packages_to_install - 1
+            else:
+                os.system("/etc/elements/repos/" + local_repo_contains + "/" + sys.argv[package_install] + "/build")
+                packages_to_install = packages_to_install - 1
+                package_install = package_install + 1
+                print(packages_to_install)
 
     elif prompt in ["n", "no"]:
         print("Exit.")
@@ -68,15 +79,30 @@ if sys.argv[1] in "install":
 
 elif sys.argv[1] in "remove":
     chk_root()
+    search_repository()
+    if len(sys.argv[2:]) != len(set(sys.argv[2:])):
+        sys.argv[2:] = list(dict.fromkeys(sys.argv[2:]))
     print("The following packages will be removed:")
     print(" " + ", ".join(sys.argv[2:]))
-    prompt = input(
+    prompt: str = input(
         "Do you wish to continue? " + "[" + Fore.GREEN + "Y" + Fore.RESET + "/" + Fore.RED + "n" + Fore.RESET + "] ")
     if prompt in ["y", "yes", ""]:
-        print()
-        ## TODO: Add removal scripts
+        packages_to_install = len(sys.argv[2:])
+        while packages_to_install > 0:
+            search_repository()
+            if pacman is True:
+                os.system("pacman -Rns --noconfirm " + sys.argv[package_install])
+                packages_to_install = packages_to_install - 1
+            else:
+                os.system("/etc/elements/repos/" + local_repo_contains + "/" + sys.argv[package_install] + "/remove")
+                packages_to_install = packages_to_install - 1
+                package_install = package_install + 1
+                print(packages_to_install)
+
     elif prompt in ["n", "no"]:
+        print("Exit.")
         sys.exit()
+
 
 elif sys.argv[1] in "search":
     search_repository()
@@ -91,7 +117,16 @@ elif sys.argv[1] in "search":
 
 
 elif sys.argv[1] in "update":
-    os.system("wget https://raw.githubusercontent.com/NitrogenLinux/elements/" + branch + "/Elements.py")
+    chk_root()
+    os.system("wget https://raw.githubusercontent.com/NitrogenLinux/elements/" + branch + "/lmt")
+    os.system("mv lmt /usr/bin/")
+    os.system("git clone https://github.com/tekq/elements-search.git")
+    os.system("mv -vf elements-search/search-repo /etc/elements/")
+    os.system("mv -vf elements-search/search /etc/elements/")
+    os.system("rm -rvf elements-search")
+    os.system("chmod +x /etc/elements/{search,search-repo}")
+    os.system("chmod +x /usr/bin/*")
+    os.system("pacman -Syu")
 ## TODO: Complete update script
 ## TODO: Make update script use the binary file instead of the source code
 
@@ -99,7 +134,8 @@ elif sys.argv[1] in "show":
     if os.system("./search " + sys.argv[2] + " >> /dev/null") != 0:
         print(sys.argv[2] + " not found.")
     else:
+        search_repository()
         print("Package: " + sys.argv[2])
-        print("Repository: " + os.popen("./search-repo " + sys.argv[2]).read())
+        print("Repository: " + local_repo_contains)
 else:
     print(sys.argv[1] + ": Command Not found.")
